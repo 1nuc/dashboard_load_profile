4ba7e3b4f84ac34e267fa48f081dcefa387f5562
1nuc <ahmedalyafai85@gmail.com>
2026-04-05 07:32:21 +0800

    save notes

--- src/pages/utility/utility.jsx
+++ src/pages/utility/utility.jsx
@@ -4,8 +4,17 @@ import './utility.css'
 export function UtilityView (){
   const location=useLocation();
   const buildings=location.state?.buildings;
+  const [searched, useSearched]=useState([]);
   return(
     <div className="buildings-panel">
+      <div className="search-bldg">
+      <h1> Select a building</h1>
+      <p> click a building to view its dashboard</p>
+            <label>
+                 <textarea className="bldg-search" value={searched} onChange={()=> setSearched()} 
+        placeholder='Search building ID, ex. 171237'/>
+            </label>
+      </div>
       {
         <ul className="building-view">
           {buildings.map((bldg, index) => (
