41fcd8650610565248d79049cf26970b61a3b5a3
1nuc <ahmedalyafai85@gmail.com>
2026-04-04 19:24:43 +0800

    update colors background

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState, useRef, useEffect } from 'react';
 import viteLogo from './assets/vite.svg';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Sidebar from './components/sidebar-component/sidebar'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { Dashboard } from './pages/dashboard/dashboard';
 
 function App() {
   return (
       <BrowserRouter>
-          <Sidebar/> 
           <Routes>
               <Route path='/dashboard' element={ <Dashboard/>}/>
           </Routes>
--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -3,18 +3,22 @@ import { AreaPlot } from '../../components/plots-component/areachart'
 import { PiePlot } from '../../components/plots-component/piechart'
 import { BarPlot} from '../../components/plots-component/barchart'
 import { DougnutPlot } from '../../components/plots-component/doughnut'
+import Sidebar from '../../components/sidebar-component/sidebar'
 // import { MultiTypePlot } from './components/plots-component/multitypeChart'
 
 export const Dashboard=()=>{
 
   return (
-    <div className= "dashboard">
-        <LinePlot/>
-        <AreaPlot/>
-        <PiePlot/>
-        <BarPlot/>
-        <DougnutPlot/>
-        {/* <MultiTypePlot/> */}
-    </div>
+    <>
+          <Sidebar/>
+      <div className= "dashboard">
+          <LinePlot/>
+          <AreaPlot/>
+          <PiePlot/>
+          <BarPlot/>
+          <DougnutPlot/>
+          {/* <MultiTypePlot/> */}
+      </div>
+    </>
   )
 }
