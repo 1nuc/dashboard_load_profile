4096614ad84f2674213f0f4071f55c491f7ed2ed
1nuc <ahmedalyafai85@gmail.com>
2026-04-08 05:25:02 +0800

    save updates

--- src/components/plots-component/lineplot.jsx
+++ src/components/plots-component/lineplot.jsx
@@ -3,19 +3,29 @@ import { useRef, useEffect } from 'react'
 
 export function LinearPlot({data}){
   const linearRef=useRef();
+  console.log(data);
+  const flatten_data=data.flatMap(d => [
+    {"timestamp": d.timestamp, "value": d.AC, "device": "AC"},
+    {"timestamp": d.timestamp, "value": d.heating, "device": "heating"},
+    {"timestamp": d.timestamp, "value": d.television, "device": "television"},
+    {"timestamp": d.timestamp, "value": d.dishwasher, "device": "dishwasher"},
+    {"timestamp": d.timestamp, "value": d.ceiling_fan, "device": "ceiling_fan"},
+    {"timestamp": d.timestamp, "value": d.refrigerator, "device": "refrigerator"},
+    {"timestamp": d.timestamp, "value": d.clothes_washer, "device": "clothes_washer"},
+  ]);
   useEffect(()=>{
-    console.log(data);
     if (!data || data.length===0) return;
     const Lineplot=Plot.plot({
       height: 200,
       width: 1200,
+      color: {legend: true},
       marks: [
-        Plot.lineY(data,{
+        Plot.lineY(flatten_data,{
           x: "timestamp",
-          y: "AC",
+          y: "value",
           interval: "day",
-          stroke: "red",
-        })
+          stroke: "device",
+        }),
       ],
       y: {grid: true},
     });
