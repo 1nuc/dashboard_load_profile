1965044cdd4c8d4796247e0676b31a3aab3b920d
1nuc <ahmedalyafai85@gmail.com>
2026-05-05 04:35:20 +0800

    save notes

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -26,7 +26,6 @@ export const Dashboard=()=>{
   useEffect(()=>{
     async function fetchData(){
       await GetPredictions({setData, building, setIsLoading});
-      setTimeout(() => setIsLoading(false), 3000);
     }
     fetchData();
   },[building]);
